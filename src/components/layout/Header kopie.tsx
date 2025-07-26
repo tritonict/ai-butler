f72bd3c0893d512f942afbe7936e3e77b02e8c67
tritonict <info@tritonict.com>
2025-07-26 10:33:45 +0200

    versie 0.4.8

--- src/components/layout/Header kopie.tsx
+++ src/components/layout/Header kopie.tsx
@@ -77,30 +77,21 @@ export default function Header({ toggleSidebar }: { toggleSidebar?: () => void }
 
   return (
   <>
-    <header className="w-full bg-gray-100 dark:bg-neutral-900 shadow-md">
-    <div className="max-w-screen-xl mx-auto flex items-center justify-between px-4 sm:px-6 h-16">
-    	<div className="flex items-center gap-3">
-    		<button onClick={toggleSidebar} className="p-2">
-        	<svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-          	<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
-        	</svg>
-      	</button>
+    <header className="overflow-x-hidden flex items-center justify-between bg-gray-100 dark:bg-neutral-900 text-zinc-900 dark:text-white px-3 sm:px-2 lg:px-4 py-2 text-sm relative">
         
-        <div className="flex items-center gap-2">
-        	<img src="/logo_black.png" alt="Logo" className="w-8 h-8" />
-        	<span className="text-lg font-bold text-neutral-800 dark:text-white">AI Butler</span>
-      	</div>
-      </div>
+          <button onClick={toggleSidebar} className="block">
+            <Menu className="w-6 h-6 shrink-0 text-neutral-800 dark:text-white" />
+          </button>
+          <img src="/logo_black.png" alt="Logo" className="h-8 w-8 shrink 0" />
+        
+        <h1 className="hidden md:block text-xl font-bold text-neutral-800 dark:text-white">AI Butler</h1>
       
-      <div className="flex items-center gap-4 text-neutral-800 dark:text-white text-sm sm:text-base">
-      	<div><span onClick={toggleView} >{tokenText} tokens</span></div>
-      	<div><span>€ {formattedSaldo}</span></div>
-    	</div>    
-             
-        
-        
+        <span onClick={toggleView} className="hidden md:block cursor-pointer hover:underline">
+          {tokenText} tokens
+        </span>
+        <span className="hidden md:block ">€ {formattedSaldo}</span>
        
-    </div>  
+      
     </header>
 		{disableActions && (
   		<div className="w-full bg-red-600 text-white text-center py-3 text-sm">
